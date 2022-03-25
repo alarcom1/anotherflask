3c288c0b34af78d000554c054d5ce272575b8dc1
mauricio <ma2546@njit.edu>
2022-03-24 22:43:16 -0400

    updated calculator program with calculator history

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -2,21 +2,26 @@
 # From specifies the namespace
 from calculator import Calculator
 
+def tuple_list():
+    """Arranging data for AAA testing"""
+    return 1.0, 2
 
 def test_calculator_add_method():
     """Testing the Calculator"""
     # this is show using the calculator object add method
-    tuple_list = (1.0, 2)
-    assert Calculator.add(tuple_list) == 3
+
+    ## Act for AAA testing
+    result = Calculator.add(tuple_list())
+
+    ## Assertion for AAA
+    assert result == 3
 
 
 def test_calculator_subtract_method():
     """Testing the Calculator Subtract"""
-    tuple_list = (1, 2)
-    assert Calculator.subtract(tuple_list) == -3
+    assert Calculator.subtract(tuple_list()) == -3
 
 
 def test_calculator_multiply_method():
     """Testing the Calculator Subtract"""
-    tuple_list = (1, 2)
-    assert Calculator.multiply(tuple_list) == 2
+    assert Calculator.multiply(tuple_list()) == 2
